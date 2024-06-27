a250d6452c389dddf9debb2527c0f3cd3575ec71
Coder Shaheer <118484201+TheHackerDude1083@users.noreply.github.com>
2024-06-26 23:05:08 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,49 @@
+# Pomodoro Timer V17 by Shaheer on Adafruit Proximity Trinkey
+
+import time
+import board
+import neopixel
+import touchio
+
+# Touch input setup
+touch_one = touchio.TouchIn(board.TOUCH1)
+touch_two = touchio.TouchIn(board.TOUCH2)
+
+# NeoPixel setup
+pixels = neopixel.NeoPixel(board.NEOPIXEL,  2)
+
+# Time durations (in seconds)
+WORK_TIME =  1 *  10  #  30 minutes  30 *  60
+SHORT_BREAK_TIME =  1 *  5  #  15 minutes  15 *  60
+LONG_BREAK_TIME =  1 *  10  #  30 minutes  30 *  60
+RGB_TIME =  10  #  10 seconds for RGB effect
+
+# State machine states
+STATE_WORK =  0
+STATE_SHORT_BREAK =  1
+STATE_LONG_BREAK =  2
+STATE_RGB =  3
+
+# Start in work state
+current_state = STATE_WORK
+
+def work_session():
+    pixels.fill((255,  255,  0))  # Yellow light for work
+    time.sleep(WORK_TIME)
+
+def short_break():
+    pixels.fill((0,  255,  255))  # Cyan light for short break
+    time.sleep(SHORT_BREAK_TIME)
+
+def long_break():
+    pixels.fill((0,  0,  255))  # Blue light for long break
+    time.sleep(LONG_BREAK_TIME)
+
+def rgb_effect():
+    for j in range(3):
+        for i in range(256):
+            pixels[0] = (i,  255,  255 - i)
+            pixels[1] = (255 - i, i,  255)
+            time.sleep(0.01)
+    pixels.fill((0,  0,  0))  # Turn off the lights after RGB effect
 
